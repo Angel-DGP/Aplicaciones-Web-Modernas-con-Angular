a12c3431eb9cc306527a28dec427edbf3257bb0a
Angel-DGP <angel.patinogd@gmail.com>
2025-11-16 15:46:10 -0500

    Reto 6 - Propiedades y plantillas

--- src/app/mi-perfil/mi-perfil.ts
+++ src/app/mi-perfil/mi-perfil.ts
@@ -6,7 +6,17 @@ import { Component } from "@angular/core";
   styleUrl: './mi-perfil.css'
 })
 export class MiPerfil {
+    public nombre: string;
+    public edad: number;
+    public ocupacion: string;
+    public direccion: string;
+    public telefono: string;
     constructor(){
+        this.nombre = "Juan Perez";
+        this.edad = 30;
+        this.ocupacion = "Desarrollador de Software";
+        this.direccion = "Av.Amazonas y Rio Coca, Quito, Ecuador";
+        this.telefono = "+593 999 999 999";
         alert("Bienvenido al perfil, estas dentro de la aplicacion!");
         console.log("Se cargo el componente MiPerfil correctamente.");
     }
