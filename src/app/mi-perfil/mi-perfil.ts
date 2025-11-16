e76795542dbf37381e24a630fe720cd9875e25bc
Angel-DGP <angel.patinogd@gmail.com>
2025-11-16 16:41:45 -0500

    Reto 8 - Multiples componentes

--- src/app/mi-perfil/mi-perfil.ts
+++ src/app/mi-perfil/mi-perfil.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
-
+import { Cuenta } from "../cuenta/cuenta";
+import { Prestamos } from "../prestamos/prestamos";
+import { Transacciones } from "../transacciones/transacciones";
 @Component({
-  selector: 'mi-perfil',
-  templateUrl: './mi-perfil.html',
-  styleUrl: './mi-perfil.css'
+    selector: 'mi-perfil',
+    imports: [Cuenta, Prestamos, Transacciones],
+    templateUrl: './mi-perfil.html',
+    styleUrl: './mi-perfil.css'
 })
 export class MiPerfil {
     public nombre: string;
@@ -11,13 +14,12 @@ export class MiPerfil {
     public ocupacion: string;
     public direccion: string;
     public telefono: string;
-    constructor(){
+    constructor() {
         this.nombre = "Juan Perez";
         this.edad = 30;
         this.ocupacion = "Desarrollador de Software";
         this.direccion = "Av.Amazonas y Rio Coca, Quito, Ecuador";
         this.telefono = "+593 999 999 999";
-        alert("Bienvenido al perfil, estas dentro de la aplicacion!");
         console.log("Se cargo el componente MiPerfil correctamente.");
     }
 }
