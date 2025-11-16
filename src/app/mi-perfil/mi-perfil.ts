ed42b398b4721479bc619e17070cacf359ede6ad
Angel-DGP <angel.patinogd@gmail.com>
2025-11-16 15:10:17 -0500

    Retp 5 - Metodo Constructor

--- src/app/mi-perfil/mi-perfil.ts
+++ src/app/mi-perfil/mi-perfil.ts
@@ -5,4 +5,9 @@ import { Component } from "@angular/core";
   templateUrl: './mi-perfil.html',
   styleUrl: './mi-perfil.css'
 })
-export class MiPerfil {}
+export class MiPerfil {
+    constructor(){
+        alert("Bienvenido al perfil, estas dentro de la aplicacion!");
+        console.log("Se cargo el componente MiPerfil correctamente.");
+    }
+}
