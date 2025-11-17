e78c214966bb704813ebffa12732ed0eb1e7c22c
Angel-DGP <angel.patinogd@gmail.com>
2025-11-16 20:13:57 -0500

    Reto 11 - Condicional IF ELSE

--- src/app/prestamos/prestamos.ts
+++ src/app/prestamos/prestamos.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './prestamos.css',
 })
 export class Prestamos {
-
+  web = "clearminds.learnworlds.com"
+  redirigir = false;
+  cambioRedireccion(){
+    this.redirigir = !this.redirigir;
+  }
 }
