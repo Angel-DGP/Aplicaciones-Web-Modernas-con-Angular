9607f5dff86f4fe3c8943d4f1448efdccd8cc9d2
Angel-DGP <angel.patinogd@gmail.com>
2025-11-18 14:33:41 -0500

    Reto 12 - Bucle FOR

--- src/app/prestamos/prestamos.ts
+++ src/app/prestamos/prestamos.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Prestamos {
   web = "clearminds.learnworlds.com"
+  prestamosList = [
+    {id: 1, nombre: "Juan Perez", monto: 5000, estado: "Aprobado"},
+    {id: 2, nombre: "Maria Gomez", monto: 3000, estado: "Pendiente"},
+    {id: 3, nombre: "Carlos Sanchez", monto: 7000, estado: "Pendiente"},
+    {id: 4, nombre: "Ana Martinez", monto: 4500, estado: "Aprobado"},
+    {id: 5, nombre: "Luis Rodriguez", monto: 6000, estado: "Pendiente"},
+    {id: 6, nombre: "Sofia Lopez", monto: 8000, estado: "Aprobado"},
+  ]
   redirigir = false;
   cambioRedireccion(){
     this.redirigir = !this.redirigir;
