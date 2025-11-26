30c92645a2e695035ddc2f9fcf6acce0d60acfdc
Angel-DGP <angel.patinogd@gmail.com>
2025-11-26 16:47:31 -0500

    Reto 22 - Paginas y rutas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { MiPerfil } from './components/mi-perfil/mi-perfil';
 import { Prestamos } from './components/prestamos/prestamos';
 
 @Component({
   selector: 'app-root',
-  imports: [Prestamos, MiPerfil],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
